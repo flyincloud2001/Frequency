88160bc0801f8af9c35d3a2998caa035fe4fa70a
flyincloud2001 <flyincloud2001@gmail.com>
2026-07-09 21:54:15 +0800

    sync: Frequency [2026-07-09 21:54:15]

--- analysis/ZEB Analysis.py
+++ analysis/ZEB Analysis.py
@@ -3,18 +3,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
 
-START = '2010-01-01'
+START = '2024-01-01'
 END = '2026-06-30'
 SYMBOL = 'ZEB.TO'
 
 raw = yf.download(SYMBOL, start=START, end=END, auto_adjust=True)
-data = raw[['Close', 'High']].copy()
-data['change'] = data['High']/data['Close'].shift(1)
+data = raw[['Close', 'Open']].copy()
+data['change'] = data['Open']/data['Close'].shift(1)
 data.dropna(inplace=True)
 
 percentages = [1.005, 1.01, 1.015, 1.02, 1.025, 1.03]
 perfs = []
 sizes = []
+higher_than_1_percent = data[data['change'] >= 1.01].index
+print(len(higher_than_1_percent))
+
 for i in percentages:
     size = ((data['change'] >= i) & (data['change'] < i+0.005)).sum()
     sizes.append(size)
@@ -25,11 +28,14 @@ for line in perfs:
 
 fig, ax = plt.subplots(figsize=(12, 5))
 ax.plot(data['change'], label='price change in %', linewidth=0.5)
+for x in higher_than_1_percent:
+    ax.axvline(x, color='r', alpha=0.5, linewidth=1, linestyle='--')
 ax.set_xlabel('dates')
 ax.set_ylabel('changes')
-ax.set_title('prices changes in percentages')
+ax.set_title('prices changes in %')
 ax.grid()
 plt.tight_layout()
+plt.savefig("prices_changes_in_%", dpi=150)
 plt.show()
 
 
